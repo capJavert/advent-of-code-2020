46f09b184768fa3321a11ab4b88600b0daadca70
capJavert <ante.baric@burza.hr>
2020-12-04 11:12:03 +0100

    feat(day-4): part 2

--- 4-day.js
+++ 4-day.js
@@ -5,6 +5,52 @@ const main = async () => {
     const data = await fetch('https://pastebin.com/raw/4K220iZj').then((response) => response.text())
     const input = data.split(/\r?\n/)
 
+    const validator = {
+        byr: {
+            test: (value) => +value >= 1920 && +value <= 2002
+        },
+        iyr: {
+            test: (value) => +value >= 2010 && +value <= 2020
+        },
+        eyr: {
+            test: (value) => +value >= 2020 && +value <= 2030
+        },
+        hgt: {
+            test: (value) => {
+                if (value.includes('cm')) {
+                    const height = +value.replace('cm', '')
+
+                    return +height >= 150 && +height <= 193
+                }
+
+                if (value.includes('in')) {
+                    const height = +value.replace('in', '')
+
+                    return +height >= 59 && +height <= 76
+                }
+
+                return false
+            }
+        },
+        hcl: {
+            test: (value) => {
+                const match = /#[0-9a-f]{6}/
+
+                return match.test(value)
+            }
+        },
+        ecl: {
+            test: (value) => ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'].includes(value)
+        },
+        pid: {
+            test: (value) => {
+                const match = /[0-9]{9}/
+
+                return match.test(value)
+            }
+        }
+    }
+
     const validPassports = input.reduce((acc, line, index) => {
         let passport = acc[acc.length - 1]
 
@@ -16,11 +62,19 @@ const main = async () => {
         if (line === '' || index === input.length - 1) {
             const passportFields = Object.keys(passport)
 
-            if (passport && !['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'].every((field) => passportFields.includes(field))) {
+            if (passport && !['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'].every((field) => {
+                if (!passportFields.includes(field)) {
+                    return false
+                }
+
+                return validator[field].test(passport[field])
+            })) {
                 acc.pop()
             }
 
-            acc.push({})
+            if (index < input.length - 1) {
+                acc.push({})
+            }
         } else {
             const newFields = line.split(' ')
             passport = newFields.forEach((field) => {
