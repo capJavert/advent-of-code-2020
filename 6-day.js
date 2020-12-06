b7dbe41b2e9299451807b1fa3663215fd7d9e7c7
capJavert <ante.baric@burza.hr>
2020-12-06 17:00:28 +0100

    feat(day-6): part 2

--- 6-day.js
+++ 6-day.js
@@ -6,18 +6,25 @@ const main = async () => {
     const input = data.split(/\r?\n/)
 
     const result = input.reduce((acc, item, index, array) => {
+        if (item === '') {
+            return acc
+        }
+
         if (!acc.groups[acc.current]) {
             acc.groups[acc.current] = {}
+            acc.members = 0
         }
 
         const group = acc.groups[acc.current]
+        acc.members += 1
 
         item.split('').forEach((answer) => {
-            group[answer] = true
+            group[answer] = (group[answer] || 0) + 1
         })
 
         if (array[index + 1] === '' || index === array.length - 1) {
-            acc.yeses += Object.keys(acc.groups[acc.current]).length
+            acc.yeses += Object.values(group)
+                .filter((count) => count === acc.members).length
             acc.current += 1
         }
 
