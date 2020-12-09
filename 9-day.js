77208b15a01d3a35401d84967257eb57f959b5f3
capJavert <ante.baric@burza.hr>
2020-12-09 14:36:54 +0100

    feat(day-9): part 2

--- 9-day.js
+++ 9-day.js
@@ -1,27 +1,34 @@
 require('es6-promise').polyfill()
 const fetch = require('isomorphic-fetch')
 
-const preambleLength = 25
-
 const main = async () => {
     const data = await fetch('https://pastebin.com/raw/30RVzPii').then((response) => response.text())
     const input = data.split(/\r?\n/)
+    const contiguousInput = 144381670 // result from part 1
 
-    const cypher = input.slice(preambleLength, input.length)
+    let result = []
+    let acc = 0
 
-    const result = cypher.find((item, index) => {
-        const preamble = input.slice(index, index + preambleLength)
-
-        return !preamble.some((a) => preamble.some((b) => {
-            if (+a === +b) {
-                return false
+    input.filter((item) => +item !== contiguousInput)
+        .some((_, index, array) => array.slice(index).some((item) => {
+            if (acc === contiguousInput) {
+                return true
             }
 
-            return +a + +b === +item
-        }))
-    })
+            result.push(+item)
+            acc += +item
 
-    console.log(result)
+            if (acc > contiguousInput) {
+                result = []
+                acc = 0
+            }
+
+            return false
+        }, 0), 0)
+
+    const sortedResult = result.sort((a, b) => a - b)
+
+    console.log(sortedResult[0] + sortedResult[sortedResult.length - 1])
 }
 
 main()
