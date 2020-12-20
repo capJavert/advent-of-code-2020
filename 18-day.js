00219867d49535c36586ba648779748ecc9a1b79
capJavert <ante.baric@burza.hr>
2020-12-20 03:27:55 +0100

    feat(day-18): part 2

--- 18-day.js
+++ 18-day.js
@@ -5,6 +5,77 @@ const main = async () => {
     const data = await fetch('https://pastebin.com/raw/B0VMkzrX').then((response) => response.text())
     const input = data.split(/\r?\n/)
 
+    const invertMath = (expression) => {
+        const { result } = expression.reduce((acc, item) => {
+            if (item === '(') {
+                acc.groups.push([])
+                acc.group = acc.groups[acc.groups.length - 1]
+
+                return acc
+            }
+
+            if (item === ')') {
+                const lastGroup = acc.groups.pop()
+                acc.group = acc.groups[acc.groups.length - 1]
+
+                if (acc.group) {
+                    acc.group.push(lastGroup)
+                } else {
+                    acc.result.push(lastGroup)
+                }
+
+                return acc
+            }
+
+            if (acc.group) {
+                acc.group.push(item)
+
+                return acc
+            }
+
+            acc.result.push(item)
+
+            return acc
+        }, {
+            result: [],
+            operator: undefined,
+            groups: [],
+            group: undefined
+        })
+
+        const inverted = []
+
+        const deepInvert = (tokens, depth = 0) => {
+            if (depth > 0) {
+                inverted.push('(')
+            }
+
+            tokens.forEach((item, index) => {
+                if (tokens[index - 1] === '+') {
+                    inverted.splice(inverted.length - 2, 0, '(')
+                    if (Array.isArray(item)) {
+                        deepInvert(item, depth + 1)
+                    } else {
+                        inverted.push(item)
+                    }
+                    inverted.push(')')
+                } else if (Array.isArray(item)) {
+                    deepInvert(item, depth + 1)
+                } else {
+                    inverted.push(item)
+                }
+            })
+
+            if (depth > 0) {
+                inverted.push(')')
+            }
+        }
+
+        deepInvert(result)
+
+        return inverted.join('').replace(/\(\)/g, '').split('')
+    }
+
     const evaluate = (expression) => {
         let initialValue
 
@@ -47,7 +118,7 @@ const main = async () => {
                 return acc
             }
 
-            acc.result = eval(`${acc.result}${acc.operator}${item}`)
+            acc.result = eval(`${acc.result}${acc.operator || '+'}${item}`)
 
             return acc
         }, {
@@ -61,7 +132,7 @@ const main = async () => {
     const result = input.reduce((acc, line) => {
         const expression = line.replace(/ /g, '').split('').map((item) => (Number.isNaN(+item) ? item : +item))
 
-        return acc + evaluate(expression).result
+        return acc + evaluate(invertMath(expression)).result
     }, 0)
 
     console.log(result)
