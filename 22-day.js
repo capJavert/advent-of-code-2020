3f547876d03fbefb0d8f1df398c5687c2a1071e0
capJavert <ante.baric@burza.hr>
2020-12-23 15:29:07 +0100

    feat(day-22): part 2

--- 22-day.js
+++ 22-day.js
@@ -1,19 +1,49 @@
 require('es6-promise').polyfill()
 const fetch = require('isomorphic-fetch')
 
-const data1 = `Player 1:
-9
-2
-6
-3
-1
+const play = (deck1, deck2) => {
+    const memo = new Set()
+    let winner
 
-Player 2:
-5
-8
-4
-7
-10`
+    while (deck1.length > 0 && deck2.length > 0) {
+        if (memo.has(`${deck1.toString()}${deck2.toString()}`)) {
+            winner = 1
+            break
+        } else {
+            memo.add(`${deck1.toString()}${deck2.toString()}`)
+        }
+
+        const draw1 = deck1.shift()
+        const draw2 = deck2.shift()
+
+        if (deck1.length >= draw1 && deck2.length >= draw2) {
+            const subWinner = play(
+                deck1.slice(0, draw1),
+                deck2.slice(0, draw2)
+            )
+
+            if (subWinner === 1) {
+                deck1.push(draw1)
+                deck1.push(draw2)
+            } else {
+                deck2.push(draw2)
+                deck2.push(draw1)
+            }
+        } else if (draw1 > draw2) {
+            deck1.push(draw1)
+            deck1.push(draw2)
+        } else {
+            deck2.push(draw2)
+            deck2.push(draw1)
+        }
+    }
+
+    if (winner === 1 || deck1.length > 0) {
+        return 1
+    }
+
+    return 2
+}
 
 const main = async () => {
     const data = await fetch('https://pastebin.com/raw/FW6fpQ70').then((response) => response.text())
@@ -36,20 +66,9 @@ const main = async () => {
         return acc
     }, { crrent: undefined })
 
-    while (deck1.length > 0 && deck2.length > 0) {
-        const draw1 = deck1.shift()
-        const draw2 = deck2.shift()
+    const winner = play(deck1, deck2)
 
-        if (draw1 > draw2) {
-            deck1.push(draw1)
-            deck1.push(draw2)
-        } else {
-            deck2.push(draw2)
-            deck2.push(draw1)
-        }
-    }
-
-    if (deck1.length > 0) {
+    if (deck1.length > 0 || winner === 1) {
         console.log(deck1.reverse().reduce((acc, item, index) => acc + item * (index + 1), 0))
     } else {
         console.log(deck2.reverse().reduce((acc, item, index) => acc + item * (index + 1), 0))
