f622fcdc30f4974bf5f74427cf5b7ace6525842d
capJavert <ante.baric@burza.hr>
2020-12-24 17:20:56 +0100

    feat(day-24): part 2

--- 24-day.js
+++ 24-day.js
@@ -7,6 +7,20 @@ const main = async () => {
 
     const grid = new Map()
 
+    const getNeighbors = (id) => {
+        const [x, y, z] = id.split(':')
+        const position = { x: +x, y: +y, z: +z }
+
+        return [
+            `${position.x + 1}:${position.y - 1}:${position.z}`,
+            `${position.x}:${position.y - 1}:${position.z + 1}`,
+            `${position.x - 1}:${position.y}:${position.z + 1}`,
+            `${position.x - 1}:${position.y + 1}:${position.z}`,
+            `${position.x}:${position.y + 1}:${position.z - 1}`,
+            `${position.x + 1}:${position.y}:${position.z - 1}`
+        ]
+    }
+
     input.forEach((item) => {
         const position = { x: 0, y: 0, z: 0 }
         let direction = ''
@@ -52,7 +66,58 @@ const main = async () => {
         const id = `${position.x}:${position.y}:${position.z}`
         const state = grid.get(id) || 'white'
         grid.set(id, state === 'black' ? 'white' : 'black')
+
+        getNeighbors(id).forEach((neighborId) => {
+            const neighborState = grid.get(neighborId)
+
+            if (!neighborState) {
+                grid.set(neighborId, 'white')
+            }
+        })
     })
+
+    for (let i = 0; i < 100; i += 1) {
+        const temp = new Map(grid)
+        const entries = temp.entries()
+        let item = entries.next()
+
+        const flip = (id, state) => {
+            const neighbors = getNeighbors(id)
+
+            const stats = neighbors.reduce((acc, neighborId) => {
+                const neighborState = temp.get(neighborId)
+
+                if (!neighborState) {
+                    grid.set(neighborId, 'white')
+                }
+
+                if (neighborState === 'black') {
+                    acc.black += 1
+                }
+
+                if (neighborState === 'white') {
+                    acc.white += 1
+                }
+
+                return acc
+            }, { black: 0, white: 0 })
+
+            if (state === 'black' && (stats.black === 0 || stats.black > 2)) {
+                grid.set(id, 'white')
+            }
+
+            if (state === 'white' && stats.black === 2) {
+                grid.set(id, 'black')
+            }
+        }
+
+        while (!item.done) {
+            const [id, state] = item.value
+            flip(id, state)
+
+            item = entries.next()
+        }
+    }
 
     const values = grid.values()
     let item = values.next()
